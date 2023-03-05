10b51acbc00eabd23531d993580efde938d6ea6a
kenny-420 <111099583+kenny-420@users.noreply.github.com>
2023-03-05 22:34:36 +1000

    Update 0033.py

--- 0033.py
+++ 0033.py
@@ -8,7 +8,7 @@ for (a,b,c,d) in product(range(1,10), repeat=4):
     x = 10*a + b
     y = 10*c + d
 
-    if d == 0 or a == b or x > y:
+    if a == b or x > y:
         continue
 
     if x/y == a/d and b == c:
